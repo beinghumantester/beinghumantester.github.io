67ac7b64e1f8f9afeee83dcc4c2d467fd65c911d
beinghumantester <automatealchemist@gmail.com>
2026-04-15 03:00:42 +0000

    Deploying to gh-pages from @ beinghumantester/beinghumantester.github.io@aeee63dc0b4c6547caf64ec52853f10695c883f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -241,16 +241,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/lessons_in_chemistry/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-when-nobody-owns-quality-making-testing-responsibility-explicit-in-open-source-projects",
+          title: 'When Nobody Owns Quality - Making Testing Responsibility Explicit in Open Source Projects...',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-community-testing-meetup-speaker-session",
+          title: 'Community Testing Meetup - Speaker Session',
+          description: "",
+          section: "News",},{id: "news-software-testing-live-episode-with-ben-dowen",
+          title: 'Software Testing Live Episode With Ben Dowen.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
