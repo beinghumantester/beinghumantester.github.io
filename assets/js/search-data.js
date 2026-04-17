fec35bd0889e0506e116c7a74955d0dd660bb07d
beinghumantester <automatealchemist@gmail.com>
2026-04-17 13:04:01 +0000

    Deploying to gh-pages from @ beinghumantester/beinghumantester.github.io@657878633edc1d03b3c34ccab0853c012ec6d0d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This page provides a summary of my experience and qualifications, along with a downloadable PDF version of my CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
